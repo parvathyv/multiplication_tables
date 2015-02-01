cdf8e773be2e6ff961b85335bf686eed2435b135
Parvathy Iyer <paro_v@yahoo.com>
2015-02-01 08:56:24 -0500

    Update multiplication_table.rb

--- lib/multiplication_table.rb
+++ lib/multiplication_table.rb
@@ -1,10 +1,7 @@
 require 'Matrix'
 
-def multiplication_table(n=12)
-
-  arr=Matrix.build(n,n) {|row,col|(row+1)*(col+1)}
-  arr=arr.to_a
-  arr.each{|row| puts "#{row.join("\t")}"}
-        
-    
+def multiplication_table(n = 12)
+  arr = Matrix.build(n,n) { |row,col| (row+1)*(col+1) }
+  arr = arr.to_a
+  arr.each { |row| puts "#{row.join("\t")}" }
 end
